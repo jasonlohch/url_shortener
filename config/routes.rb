8246f1824f6737729a90f09e1d39b650612c57ed
NextAcademy_Mac <erwhey@gmail.com>
2017-08-04 16:24:30 +0800

    set up resources for urls and set url/index as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+    get 'urls/index'
+      resources :urls
+
+    root 'urls#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
